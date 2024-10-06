971bd886087216bd3eb892aa6c67ad58577b5d37
Terry Stifler Ramírez <terryman42@gmail.com>
2024-10-05 21:48:46 -0600

    informes venta y compras

---
+++ FarmaciaElPorvenir/Reportes/InformeVentas.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace FarmaciaElPorvenir.Reportes
+{
+    partial class InformeVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        ///
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions windowsUIButtonImageOptions1 = new DevExpress.XtraBars.Docking2010.WindowsUIButtonImageOptions();
+            this.layoutControl = new DevExpress.XtraLayout.LayoutControl();
+            this.labelControl = new DevExpress.XtraEditors.LabelControl();
+            this.gridControl = new DevExpress.XtraGrid.GridControl();
+            this.xpCollectionVentas = new DevExpress.Xpo.XPCollection(this.components);
+            this.unitOfWork1 = new DevExpress.Xpo.UnitOfWork(this.components);
+            this.gridView = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colFecha = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colNo_Factura = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTotal_Factura = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTotal_IVA = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.layoutControlGroup = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.itemLabel = new DevExpress.XtraLayout.LayoutControlItem();
+            this.itemGrid = new DevExpress.XtraLayout.LayoutControlItem();
+            this.windowsUIButtonPanel = new DevExpress.XtraBars.Docking2010.WindowsUIButtonPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl)).BeginInit();
+            this.layoutControl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xpCollectionVentas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.unitOfWork1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemLabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl
+            // 
+            this.layoutControl.AllowCustomization = false;
+            this.layoutControl.Controls.Add(this.labelControl);
+            this.layoutControl.Controls.Add(this.gridControl);
+            this.layoutControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl.Name = "layoutControl";
+            this.layoutControl.Root = this.layoutControlGroup;
+            this.layoutControl.Size = new System.Drawing.Size(784, 501);
+            this.layoutControl.TabIndex = 1;
+            // 
+            // labelControl
+            // 
+            this.labelControl.AllowHtmlString = true;
+            this.labelControl.Appearance.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelControl.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(140)))), ((int)(((byte)(140)))));
+            this.labelControl.Appearance.Options.UseFont = true;
+            this.labelControl.Appearance.Options.UseForeColor = true;
+            this.labelControl.Appearance.Options.UseTextOptions = true;
+            this.labelControl.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Near;
+            this.labelControl.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.Vertical;
+            this.labelControl.Location = new System.Drawing.Point(40, 0);
+            this.labelControl.Name = "labelControl";
+            this.labelControl.Padding = new System.Windows.Forms.Padding(0, 3, 13, 6);
+            this.labelControl.Size = new System.Drawing.Size(704, 34);
+            this.labelControl.StyleController = this.layoutControl;
+            this.labelControl.TabIndex = 4;
+            this.labelControl.Text = "Informe Ventas";
+            // 
+            // gridControl
+            // 
+            this.gridControl.DataSource = this.xpCollectionVentas;
+            this.gridControl.Location = new System.Drawing.Point(40, 34);
+            this.gridControl.MainView = this.gridView;
+            this.gridControl.Name = "gridControl";
+            this.gridControl.Size = new System.Drawing.Size(704, 467);
+            this.gridControl.TabIndex = 2;
+            this.gridControl.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView});
+            // 
+            // xpCollectionVentas
+            // 
+            this.xpCollectionVentas.ObjectType = typeof(FarmaciaElPorvenir.Database.Factura_venta);
+            this.xpCollectionVentas.Session = this.unitOfWork1;
+            // 
+            // gridView
+            // 
+            this.gridView.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colId,
+            this.gridColumn1,
+            this.gridColumn2,
+            this.colFecha,
+            this.colNo_Factura,
+            this.gridColumn3,
+            this.gridColumn4,
+            this.colTotal_Factura,
+            this.colTotal_IVA});
+            this.gridView.GridControl = this.gridControl;
+            this.gridView.GroupSummary.AddRange(new DevExpress.XtraGrid.GridSummaryItem[] {
+            new DevExpress.XtraGrid.GridGroupSummaryItem(DevExpress.Data.SummaryItemType.None, "", null, "")});
+            this.gridView.Name = "gridView";
+            this.gridView.OptionsBehavior.Editable = false;
+            this.gridView.OptionsCustomization.AllowColumnMoving = false;
+            this.gridView.OptionsCustomization.AllowGroup = false;
+            this.gridView.OptionsCustomization.AllowQuickHideColumns = false;
+            this.gridView.OptionsMenu.EnableColumnMenu = false;
+            this.gridView.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gridView.OptionsView.ShowFilterPanelMode = DevExpress.XtraGrid.Views.Base.ShowFilterPanelMode.Never;
+            this.gridView.OptionsView.ShowGroupPanel = false;
+            this.gridView.OptionsView.ShowIndicator = false;
+            this.gridView.SortInfo.AddRange(new DevExpress.XtraGrid.Columns.GridColumnSortInfo[] {
+            new DevExpress.XtraGrid.Columns.GridColumnSortInfo(this.gridColumn2, DevExpress.Data.ColumnSortOrder.Descending)});
+            // 
+            // colId
+            // 
+            this.colId.FieldName = "Id";
+            this.colId.Name = "colId";
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.FieldName = "Id_Empleado!";
+            this.gridColumn1.Name = "gridColumn1";
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "Empleado";
+            this.gridColumn2.FieldName = "Id_Empleado.Nombre_Completo";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 0;
+            // 
+            // colFecha
+            // 
+            this.colFecha.FieldName = "Fecha";
+            this.colFecha.Name = "colFecha";
+            this.colFecha.Visible = true;
+            this.colFecha.VisibleIndex = 1;
+            // 
+            // colNo_Factura
+            // 
+            this.colNo_Factura.Caption = "N° Factura";
+            this.colNo_Factura.FieldName = "No_Factura";
+            this.colNo_Factura.Name = "colNo_Factura";
+            this.colNo_Factura.Visible = true;
+            this.colNo_Factura.VisibleIndex = 2;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.FieldName = "Id_Cliente!";
+            this.gridColumn3.Name = "gridColumn3";
+            // 
+            // gridColumn4
+            // 
+            this.gridColumn4.Caption = "Cliente";
+            this.gridColumn4.FieldName = "Id_Cliente.Nombre_Completo";
+            this.gridColumn4.Name = "gridColumn4";
+            this.gridColumn4.Visible = true;
+            this.gridColumn4.VisibleIndex = 3;
+            // 
+            // colTotal_Factura
+            // 
+            this.colTotal_Factura.Caption = "Total Facturas";
+            this.colTotal_Factura.FieldName = "Total_Factura";
+            this.colTotal_Factura.Name = "colTotal_Factura";
+            this.colTotal_Factura.Visible = true;
+            this.colTotal_Factura.VisibleIndex = 4;
+            // 
+            // colTotal_IVA
+            // 
+            this.colTotal_IVA.Caption = "IVA";
+            this.colTotal_IVA.FieldName = "Total_IVA";
+            this.colTotal_IVA.Name = "colTotal_IVA";
+            this.colTotal_IVA.Visible = true;
+            this.colTotal_IVA.VisibleIndex = 5;
+            // 
+            // layoutControlGroup
+            // 
+            this.layoutControlGroup.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup.GroupBordersVisible = false;
+            this.layoutControlGroup.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.itemLabel,
+            this.itemGrid});
+            this.layoutControlGroup.Name = "layoutControlGroup";
+            this.layoutControlGroup.Padding = new DevExpress.XtraLayout.Utils.Padding(40, 40, 0, 0);
+            this.layoutControlGroup.Size = new System.Drawing.Size(784, 501);
+            this.layoutControlGroup.TextVisible = false;
+            // 
+            // itemLabel
+            // 
+            this.itemLabel.Control = this.labelControl;
+            this.itemLabel.Location = new System.Drawing.Point(0, 0);
+            this.itemLabel.Name = "itemLabel";
+            this.itemLabel.Padding = new DevExpress.XtraLayout.Utils.Padding(0, 0, 0, 0);
+            this.itemLabel.Size = new System.Drawing.Size(704, 34);
+            this.itemLabel.TextSize = new System.Drawing.Size(0, 0);
+            this.itemLabel.TextVisible = false;
+            // 
+            // itemGrid
+            // 
+            this.itemGrid.Control = this.gridControl;
+            this.itemGrid.Location = new System.Drawing.Point(0, 34);
+            this.itemGrid.Name = "itemGrid";
+            this.itemGrid.Padding = new DevExpress.XtraLayout.Utils.Padding(0, 0, 0, 0);
+            this.itemGrid.Size = new System.Drawing.Size(704, 467);
+            this.itemGrid.TextSize = new System.Drawing.Size(0, 0);
+            this.itemGrid.TextVisible = false;
+            // 
+            // windowsUIButtonPanel
+            // 
+            this.windowsUIButtonPanel.AppearanceButton.Hovered.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(130)))), ((int)(((byte)(130)))));
+            this.windowsUIButtonPanel.AppearanceButton.Hovered.FontSizeDelta = -1;
+            this.windowsUIButtonPanel.AppearanceButton.Hovered.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(130)))), ((int)(((byte)(130)))));
+            this.windowsUIButtonPanel.AppearanceButton.Hovered.Options.UseBackColor = true;
+            this.windowsUIButtonPanel.AppearanceButton.Hovered.Options.UseFont = true;
+            this.windowsUIButtonPanel.AppearanceButton.Hovered.Options.UseForeColor = true;
+            this.windowsUIButtonPanel.AppearanceButton.Normal.FontSizeDelta = -1;
+            this.windowsUIButtonPanel.AppearanceButton.Normal.Options.UseFont = true;
+            this.windowsUIButtonPanel.AppearanceButton.Pressed.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.windowsUIButtonPanel.AppearanceButton.Pressed.FontSizeDelta = -1;
+            this.windowsUIButtonPanel.AppearanceButton.Pressed.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.windowsUIButtonPanel.AppearanceButton.Pressed.Options.UseBackColor = true;
+            this.windowsUIButtonPanel.AppearanceButton.Pressed.Options.UseFont = true;
+            this.windowsUIButtonPanel.AppearanceButton.Pressed.Options.UseForeColor = true;
+            this.windowsUIButtonPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
+            windowsUIButtonImageOptions1.ImageUri.Uri = "Preview;Size32x32;GrayScaled";
+            this.windowsUIButtonPanel.Buttons.AddRange(new DevExpress.XtraEditors.ButtonPanel.IBaseButton[] {
+            new DevExpress.XtraBars.Docking2010.WindowsUIButton("Print", true, windowsUIButtonImageOptions1, DevExpress.XtraBars.Docking2010.ButtonStyle.PushButton, "", -1, true, null, true, false, true, null, -1, false)});
+            this.windowsUIButtonPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.windowsUIButtonPanel.EnableImageTransparency = true;
+            this.windowsUIButtonPanel.ForeColor = System.Drawing.Color.White;
+            this.windowsUIButtonPanel.Location = new System.Drawing.Point(0, 501);
+            this.windowsUIButtonPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.windowsUIButtonPanel.MaximumSize = new System.Drawing.Size(0, 60);
+            this.windowsUIButtonPanel.MinimumSize = new System.Drawing.Size(60, 60);
+            this.windowsUIButtonPanel.Name = "windowsUIButtonPanel";
+            this.windowsUIButtonPanel.Size = new System.Drawing.Size(784, 60);
+            this.windowsUIButtonPanel.TabIndex = 5;
+            this.windowsUIButtonPanel.Text = "windowsUIButtonPanel";
+            this.windowsUIButtonPanel.UseButtonBackgroundImages = false;
+            this.windowsUIButtonPanel.ButtonClick += new DevExpress.XtraBars.Docking2010.ButtonEventHandler(this.windowsUIButtonPanel_ButtonClick);
+            // 
+            // InformeVentas
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.layoutControl);
+            this.Controls.Add(this.windowsUIButtonPanel);
+            this.Name = "InformeVentas";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl)).EndInit();
+            this.layoutControl.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xpCollectionVentas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.unitOfWork1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemLabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl;
+        private DevExpress.XtraEditors.LabelControl labelControl;
+        private DevExpress.XtraGrid.GridControl gridControl;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup;
+        private DevExpress.XtraLayout.LayoutControlItem itemLabel;
+        private DevExpress.XtraLayout.LayoutControlItem itemGrid;
+        private DevExpress.XtraBars.Docking2010.WindowsUIButtonPanel windowsUIButtonPanel;
+        private DevExpress.Xpo.UnitOfWork unitOfWork1;
+        private DevExpress.Xpo.XPCollection xpCollectionVentas;
+        private DevExpress.XtraGrid.Columns.GridColumn colId;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn colFecha;
+        private DevExpress.XtraGrid.Columns.GridColumn colNo_Factura;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+        private DevExpress.XtraGrid.Columns.GridColumn colTotal_Factura;
+        private DevExpress.XtraGrid.Columns.GridColumn colTotal_IVA;
+    }
+}
