66066252f564a5cd5c0e4f9c942e1c51f243879a
elmer1292 <elmermat321@gmail.com>
2024-10-23 23:04:58 -0600

    graficos
    no terminados

---
+++ FarmaciaElPorvenir/Graficos.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FarmaciaElPorvenir
+{
+    partial class Graficos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.GraficoVentaSemana = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.GraficoVendedorSemana = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.GraficoVentaSemana)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GraficoVendedorSemana)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GraficoVentaSemana
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.GraficoVentaSemana.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.GraficoVentaSemana.Legends.Add(legend1);
+            this.GraficoVentaSemana.Location = new System.Drawing.Point(29, 27);
+            this.GraficoVentaSemana.Name = "GraficoVentaSemana";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.GraficoVentaSemana.Series.Add(series1);
+            this.GraficoVentaSemana.Size = new System.Drawing.Size(300, 300);
+            this.GraficoVentaSemana.TabIndex = 0;
+            this.GraficoVentaSemana.Text = "chart1";
+            this.GraficoVentaSemana.Click += new System.EventHandler(this.GraficoVentaSemana_Click);
+            // 
+            // GraficoVendedorSemana
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.GraficoVendedorSemana.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.GraficoVendedorSemana.Legends.Add(legend2);
+            this.GraficoVendedorSemana.Location = new System.Drawing.Point(402, 36);
+            this.GraficoVendedorSemana.Name = "GraficoVendedorSemana";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.GraficoVendedorSemana.Series.Add(series2);
+            this.GraficoVendedorSemana.Size = new System.Drawing.Size(300, 300);
+            this.GraficoVendedorSemana.TabIndex = 1;
+            this.GraficoVendedorSemana.Text = "chart1";
+            // 
+            // Graficos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GraficoVendedorSemana);
+            this.Controls.Add(this.GraficoVentaSemana);
+            this.Name = "Graficos";
+            this.Text = "Graficos";
+            ((System.ComponentModel.ISupportInitialize)(this.GraficoVentaSemana)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GraficoVendedorSemana)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart GraficoVentaSemana;
+        private System.Windows.Forms.DataVisualization.Charting.Chart GraficoVendedorSemana;
+    }
+}
---
+++ FarmaciaElPorvenir/Graficos.cs
@@ -0,0 +1,97 @@
+﻿using DevExpress.XtraCharts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FarmaciaElPorvenir
+{
+    public partial class Graficos : Form
+    {
+        public Graficos()
+        {
+            InitializeComponent();
+        }
+
+        private void GraficoVentaSemana_Click(object sender, EventArgs e)
+        {
+            // Cargar datos de la base de datos
+            var productos = LoadProductsFromDatabase();
+            var vendedores = LoadVendorsFromDatabase();
+
+            // Configurar gráfico de productos más vendidos
+            GraficoVentaSemana.Series.Clear();
+            Series series = new Series("Productos Vendidos", ViewType.Bar);
+            foreach (var producto in productos)
+            {
+                series.Points.Add(new SeriesPoint(producto.Key, producto.Value));
+            }
+            GraficoVentaSemana.Series.Add(series);
+
+            // Configurar gráfico de vendedor más vendido
+            GraficoVendedorSemana.Series.Clear();
+            Series series2 = new Series("Ventas por Vendedor", ViewType.Pie);
+            foreach (var vendedor in vendedores)
+            {
+                series2.Points.Add(new SeriesPoint(vendedor.Key, vendedor.Value));
+            }
+            GraficoVendedorSemana.Series.Add(series2);
+        }
+
+        private Dictionary<string, int> LoadProductsFromDatabase()
+        {
+            var productos = new Dictionary<string, int>();
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+            string query = "SELECT NombreProducto, SUM(CantidadVendida) AS TotalVendida FROM Ventas WHERE Fecha >= DATEADD(week, -1, GETDATE()) GROUP BY NombreProducto";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string nombre = reader["NombreProducto"].ToString();
+                        int cantidadVendida = Convert.ToInt32(reader["TotalVendida"]);
+                        productos[nombre] = cantidadVendida;
+                    }
+                }
+            }
+
+            return productos;
+        }
+
+        private Dictionary<string, int> LoadVendorsFromDatabase()
+        {
+            var vendedores = new Dictionary<string, int>();
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+            string query = "SELECT NombreVendedor, SUM(CantidadVendida) AS TotalVendida FROM Ventas WHERE Fecha >= DATEADD(week, -1, GETDATE()) GROUP BY NombreVendedor";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string nombre = reader["NombreVendedor"].ToString();
+                        int cantidadVendida = Convert.ToInt32(reader["TotalVendida"]);
+                        vendedores[nombre] = cantidadVendida;
+                    }
+                }
+            }
+
+            return vendedores;
+        }
+    }
+}
